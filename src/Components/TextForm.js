5aad5db9fe45263b296602612e62c1830c647d6e
DeepanshuChaudhary01 <deepanshu123chaudhary@gmail.com>
2023-03-03 02:36:39 +0530

    updating code

--- src/Components/TextForm.js
+++ src/Components/TextForm.js
@@ -68,7 +68,7 @@ const HandleReverseClick=()=>{//using arrow function
     </div>
     <div className='container' style={{color: props.theme==='light'?'black':'white'}}>
       <h2>Your Text Summary</h2>
-      <p>{text.split(" ").length} and {text.length}</p>
+      <p>{text.split(" ").length} words and {text.length} characters</p>
       <h1>Preview</h1>
       <p>{text.length>0?text:"Enter something in textbox to preview it here"}</p>
     </div>
